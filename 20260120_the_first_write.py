9bdcf1c5d6d188372e1eda9e77d9ba26f85f6f6e
yangxy9211 <yangxy9211@gmail.com>
2026-01-20 14:01:55 +0800

    another change ...

--- 20260120_the_first_write.py
+++ 20260120_the_first_write.py
@@ -2,4 +2,9 @@ print("this is my first coding doc")
 # ======= This is the change ========
 print("Now I add my second line.")
 print("Do you hear it?")
+# ======= This is another change ========
+print("processing 1...")
+print("processing 2...")
+print("processing 3...")
+print("please wait ...")
 
