d25d1cb5691033bd7ed27249431f88e3a15bbe50
yangxy9211 <yangxy9211@gmail.com>
2026-01-20 14:35:08 +0800

    reset mymath calculate

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ print("this is my first coding doc")
 # ======= This is the change ========
 print("Now I add my second line.")
 print("Do you hear it?")
-# ======= This is another change ========
+# ======= This is another change ===
 print("processing 1...")
 print("processing 2...")
 print("processing 3...")
@@ -14,3 +14,5 @@ a = 10
 b = 3
 c = calculate(a,b)
 print(c)
+# ======= Calculate 11 and 12 =========
+print(calculate(11, 12))
