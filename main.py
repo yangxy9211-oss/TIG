9e6b32fbd06cbaa823b1d96643afd92b22db5b79
yangxy9211 <yangxy9211@gmail.com>
2026-01-20 14:12:43 +0800

    change the first file to main; add mymyth.py; add calculate from mymath.py to main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from mymath import calculate
+
 print("this is my first coding doc")
 # ======= This is the change ========
 print("Now I add my second line.")
@@ -7,4 +9,8 @@ print("processing 1...")
 print("processing 2...")
 print("processing 3...")
 print("please wait ...")
-
+# ======= Calculate a and b =========
+a = 10
+b = 3
+c = calculate(a,b)
+print(c)
---
+++ mymath.py
@@ -0,0 +1,3 @@
+def calculate(a, b):
+    return (a + b) * (a + b) * (a + b)
+
